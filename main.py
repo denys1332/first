2075c68e685c576b32e5ef04347e820e168915bb
Denys Ivanov <enigma.uk13@gmail.com>
2022-10-17 17:55:37 +0300

    docker

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print"hello"
+print("hello")
+a = 10
+login = input("Your login ?")
+print(login)
+password = input("Your password ?")
+password2 = input("Repeat your password")
+if password == password2:
+    print("Cool!")
+    print(a)
+else:
+    print("some problems")
+range{1.3}
